300b26bf178eb60b943d372bf58d551db7740bb3
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-07 10:05:32 +0700

    menambahkan Scaffold sebagai struktur layout utama dan menambahkan TopAppBar dengan title dari string resource

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,8 +1,16 @@
 package com.example.navigasiku.view
 
-import android.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.navigasiku.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +18,14 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(id = R.string.home), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.black))
+            ) }
+    ) {}
+
 
 }
