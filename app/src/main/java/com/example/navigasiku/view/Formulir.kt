f1d083655747300bc1fd3aa6e4811abc033a1e24
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-07 09:48:17 +0700

    membuat komponen FormIsian dengan parameter jenisK dan callback OnSubmitBtnClick

---
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -0,0 +1,14 @@
+package com.example.navigasiku.view
+
+import android.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+}
